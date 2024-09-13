a28d1bf466ece2396f1d6a5ee7c1b143b4a5340a
Kristianonmopk <elliskristiano5@gmail.com>
2024-09-13 14:30:58 +0300

    Create difference between divide and floor division.py

---
+++ difference between divide and floor division.py
@@ -0,0 +1,8 @@
+#When performing the operation 10/3 the result is 3.3333333333333335. 
+
+#However, when performing the operation 10//3 the result is 3.
+
+#The // operator performs a floor division, which means that it returns the largest possible integer less than or equal to the result of the division. In this case, the result of 10//3 is 3, which is the largest integer less than or equal to 3.3333333333333335.
+
+#However, an example where the // method would get a result equal to the result ofthe regular / is when there is no decimal in the result of the division.
+#EG. 4 // 2 = 2
